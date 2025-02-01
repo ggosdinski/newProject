38792548c093e6fe78fad64ff60f57fd89c0b698
ggosdinski <gianfranco.gosdinski@icloud.com>
2025-02-01 13:17:03 -0500

    callbackURL correction

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const Movie = mongoose.model('Movie', movieSchema);
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/github/callback"
+  callbackURL: "https://newproject-dlxk.onrender.com/auth/github/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
